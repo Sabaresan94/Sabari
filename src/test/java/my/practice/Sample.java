552668558508ffc4e84ff4c7ebd1f4e9d17659e4
Sabaresan94 <sabaresan94@gmail.com>
2024-09-21 23:52:34 +0530

    samplegitpush

---
+++ src/test/java/my/practice/Sample.java
@@ -0,0 +1,11 @@
+package my.practice;
+
+public class Sample {
+	public void tamil() {
+		System.out.println("mark is 100");
+	}
+	
+	public void english() {
+		System.out.println("mark is 99");
+	}
+}
