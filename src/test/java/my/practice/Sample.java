c7b6758e5df6a142244c6be15301d2c88d40c8c7
Sabaresan94 <sabaresan94@gmail.com>
2024-09-22 10:46:34 +0530

    two methods added

--- src/test/java/my/practice/Sample.java
+++ src/test/java/my/practice/Sample.java
@@ -6,6 +6,12 @@ public class Sample {
 	}
 	
 	public void english() {
+		System.out.println("mark is 85");
+	}
+	public void maths() {
 		System.out.println("mark is 99");
+	}
+	public void science() {
+		System.out.println("mark is 80");
 	}
 }
